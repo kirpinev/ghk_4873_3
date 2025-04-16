2fa63e5a083a8891b3be62b642de7e78a278ac59
Igor Kirpinev <kirpinevigor@yandex.ru>
2025-04-16 11:55:00 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -309,7 +309,7 @@ export const App = () => {
           view="medium"
           className={appSt.productsTitle}
         >
-          Деньги под рукой
+          Альфа-лимит
         </Typography.TitleResponsive>
         <Gap size={8} />
         <Typography.Text
